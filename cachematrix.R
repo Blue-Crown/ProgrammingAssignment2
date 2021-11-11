acc91f077e97eac1bbebb04cbd038d876c50ef7b
BlueCrown <85197133+Blue-Crown@users.noreply.github.com>
2021-11-11 14:55:00 -0800

    My solution to cachematrix.R
    First upload of solution to Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function creates a special "matrix" (really a list)
+## of four sub-functions:
+## 1) setmatrix: creates a matrix 'a'
+## 2) getmatrix: returns matrix 'a'
+## 3) setinv: sets the value to the inverse of matrix 'a', named 'q'
+## 4) getinv: returns the value of 'q'
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(a = matrix()) {
+  q <- NULL     ##creates empty variable 'q',the inverse matrix of 'x'
+  setmatrix <- function(b) {
+    a <<- b     ##sets 'a' to the value of 'b'
+    q <<- NULL  ##resets 'q' to an empty variable
+  }
+  getmatrix <- function() a  ##returns the matrix 'a'
+  setinv <- function(inv) q <<- inv  #sets an inverse matrix 'inv' to value 'q'
+  getinv <- function() q ## returns the value of  the inverse matrix 'q'
+  list(setmatrix = setmatrix, getmatrix = getmatrix, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function returns the value of 'q', 'x's inverse matrix, by 
+## first checking if a value has been cached already and, if that exists,
+## returning that. If no value has been cached, it will calculate the inverse 
+## matrix using solve() and return that.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a, ...) {
+  q <- a$getinv()   ##returns the inverse matrix 'q' from getinv
+  if(!is.null(q)) {
+    message("Retrieving cached data. BEEP BOOP.")
+    return(q)
+    ## returns a message if a the inverse matrix q has been previously 
+    ## determined, and return cached value of 'q'
+  }
+  else{
+    matrixA <- a$getmatrix()  ##call forward the matrix 'A'
+    if(nrow(matrixA) != ncol(matrixA)) {
+      message("This matrix is not square, fool.")
+    }
+    else{
+      q <- solve(matrixA, ...) ##calculates the inverse of matrix 'A', and calls it 'q'
+      a$setinv(q) ##set the inverse of 'A' as 'q'
+      message("Inverse matrix successfully calculated! BEEP BOOP.")
+      return(q)
+    }
+  }
 }
+
